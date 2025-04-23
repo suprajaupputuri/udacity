5edc262902ae17b8bc69951528f982bb6fea3bbf
suprajaupputuri <128447615+suprajaupputuri@users.noreply.github.com>
2025-04-23 23:48:57 +0530

    Update accelerometer_landing_to_trusted.py

--- accelerometer_landing_to_trusted.py
+++ accelerometer_landing_to_trusted.py
@@ -20,14 +20,14 @@ DEFAULT_DATA_QUALITY_RULESET = """
     ]
 """
 
-# Script generated for node Accelerometer Landing
-AccelerometerLanding_node1745414943311 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="accelerometer_landing", transformation_ctx="AccelerometerLanding_node1745414943311")
-
 # Script generated for node Customer Trusted
-CustomerTrusted_node1745414991849 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="customer_trusted", transformation_ctx="CustomerTrusted_node1745414991849")
+CustomerTrusted_node1745431881793 = glueContext.create_dynamic_frame.from_options(format_options={"multiLine": "false"}, connection_type="s3", format="json", connection_options={"paths": ["s3://stedi-supraja/customer/trusted/"], "recurse": True}, transformation_ctx="CustomerTrusted_node1745431881793")
+
+# Script generated for node Accelerometer Landing
+AccelerometerLanding_node1745431882707 = glueContext.create_dynamic_frame.from_options(format_options={"multiLine": "false"}, connection_type="s3", format="json", connection_options={"paths": ["s3://stedi-supraja/accelerometer/landing/"], "recurse": True}, transformation_ctx="AccelerometerLanding_node1745431882707")
 
 # Script generated for node Join
-Join_node1745415021765 = Join.apply(frame1=CustomerTrusted_node1745414991849, frame2=AccelerometerLanding_node1745414943311, keys1=["email"], keys2=["user"], transformation_ctx="Join_node1745415021765")
+Join_node1745415021765 = Join.apply(frame1=CustomerTrusted_node1745431881793, frame2=AccelerometerLanding_node1745431882707, keys1=["email"], keys2=["user"], transformation_ctx="Join_node1745415021765")
 
 # Script generated for node Drop Fields
 DropFields_node1745415057314 = DropFields.apply(frame=Join_node1745415021765, paths=["email", "phone"], transformation_ctx="DropFields_node1745415057314")
