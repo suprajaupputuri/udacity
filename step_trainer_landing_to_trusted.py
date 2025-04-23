d44d8cdde22b6f131884a052765a2f40beb35791
suprajaupputuri <128447615+suprajaupputuri@users.noreply.github.com>
2025-04-23 23:59:20 +0530

    Update step_trainer_landing_to_trusted.py

--- step_trainer_landing_to_trusted.py
+++ step_trainer_landing_to_trusted.py
@@ -26,11 +26,11 @@ DEFAULT_DATA_QUALITY_RULESET = """
     ]
 """
 
-# Script generated for node step_trainer_landing
-step_trainer_landing_node1745419311868 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="step_trainer_landing", transformation_ctx="step_trainer_landing_node1745419311868")
+# Script generated for node Customer Curated
+CustomerCurated_node1745432654268 = glueContext.create_dynamic_frame.from_options(format_options={"multiLine": "false"}, connection_type="s3", format="json", connection_options={"paths": ["s3://stedi-supraja/customer/curated/"], "recurse": True}, transformation_ctx="CustomerCurated_node1745432654268")
 
-# Script generated for node customer_curated
-customer_curated_node1745419350388 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="customer_curated", transformation_ctx="customer_curated_node1745419350388")
+# Script generated for node step_trainer_landing
+step_trainer_landing_node1745432782380 = glueContext.create_dynamic_frame.from_options(format_options={"multiLine": "false"}, connection_type="s3", format="json", connection_options={"paths": ["s3://stedi-supraja/step_trainer/landing/"], "recurse": True}, transformation_ctx="step_trainer_landing_node1745432782380")
 
 # Script generated for node SQL Query
 SqlQuery0 = '''
@@ -38,9 +38,8 @@ select step_trainer_landing.*
 from step_trainer_landing
 inner join customer_curated
 on step_trainer_landing.serialNumber = customer_curated.serialNumber
-
 '''
-SQLQuery_node1745419383320 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"customer_curated":customer_curated_node1745419350388, "step_trainer_landing":step_trainer_landing_node1745419311868}, transformation_ctx = "SQLQuery_node1745419383320")
+SQLQuery_node1745419383320 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"customer_curated":CustomerCurated_node1745432654268, "step_trainer_landing":step_trainer_landing_node1745432782380}, transformation_ctx = "SQLQuery_node1745419383320")
 
 # Script generated for node step_trainer_trusted
 EvaluateDataQuality().process_rows(frame=SQLQuery_node1745419383320, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1745419002129", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
